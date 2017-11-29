cc8478c7a8feb61413db781e7652d3608be6e9c0
Matjaz Tome <matjaz.tome@siworks.eu>
2017-11-29 12:59:30 +0100

    Added al required libraries from ASF

--- Daq/src/asf.h
+++ Daq/src/asf.h
@@ -52,6 +52,9 @@
  * running the ASF driver selector tool. Any changes will be discarded.
  */
 
+// From module: ADC - Analog-to-digital Converter
+#include <adc.h>
+
 // From module: Common SAM compiler driver
 #include <compiler.h>
 #include <status_codes.h>
@@ -59,16 +62,64 @@
 // From module: Common build items for user board support templates
 #include <user_board.h>
 
+// From module: DACC - Digital-to-Analog Converter
+#include <dacc.h>
+
 // From module: Generic board support
 #include <board.h>
 
+// From module: IOPORT - General purpose I/O service
+#include <ioport.h>
+
 // From module: Interrupt management - SAM implementation
 #include <interrupt.h>
+
+// From module: MATRIX - Bus Matrix
+#include <matrix.h>
+
+// From module: PDC - Peripheral DMA Controller Example
+#include <pdc.h>
+
+// From module: PIO - Parallel Input/Output Controller
+#include <pio.h>
+
+// From module: PMC - Power Management Controller
+#include <pmc.h>
+#include <sleep.h>
 
 // From module: Part identification macros
 #include <parts.h>
 
 // From module: SAM3S startup code
 #include <exceptions.h>
+
+// From module: SUPC - Supply Controller
+#include <supc.h>
+
+// From module: Sleep manager - SAM implementation
+#include <sam/sleepmgr.h>
+#include <sleepmgr.h>
+
+// From module: System Clock Control - SAM3S implementation
+#include <sysclk.h>
+
+// From module: TC - Timer Counter
+#include <tc.h>
+
+// From module: USB CDC Protocol
+#include <usb_protocol_cdc.h>
+
+// From module: USB Device CDC (Single Interface Device)
+#include <udi_cdc.h>
+
+// From module: USB Device Stack Core (Common API)
+#include <udc.h>
+#include <udd.h>
+
+// From module: WDT - Watchdog Timer
+#include <wdt.h>
+
+// From module: pio_handler support enabled
+#include <pio_handler.h>
 
 #endif // ASF_H
